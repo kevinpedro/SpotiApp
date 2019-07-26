c2a4beba771f9147fc0aef4949a2588ad926f1ab
kevinpedro <pedro.heredia@alumnos.udg.mx>
2019-07-26 12:44:52 -0500

    Proyecto Spotify API

---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+
+  constructor( private http: HttpClient ) {
+    console.log('Spotify Service Listo')
+  }
+
+  getQuery(query:string){
+
+      const url = `https://api.spotify.com/v1/${ query }`;
+
+      const headers =  new HttpHeaders({
+        'Authorization': 'Bearer BQDcPSvIbJJ1avYu_wJvaO7qXdKm_eqki5WpT06Nsmq5oBaX2_RIYQaqsUHpzOeuu8HSIyIJ2BxgRT8r7Yo'
+      });
+
+      return this.http.get(url, {headers});
+  }
+
+  getNewleases(){
+      return this.getQuery('browse/new-releases')
+                 .pipe( map( data =>data['albums'].items ) );
+  }
+
+  getArtistas(termino:string){
+      return this.getQuery(`search?q=${ termino }&type=artist&limit=15`)
+                 .pipe( map( data =>data['artists'].items));
+  }
+
+  getArtista( id:string){
+      return this.getQuery(`artists/${id}` )
+                 //.pipe( map( data =>data['artists'.items));
+  }
+
+  getTopTracks( id:string){
+      return this.getQuery(`artists/${id}/top-tracks?country=us`)
+                 .pipe( map( data =>data['tracks']));
+  }
+
+}
